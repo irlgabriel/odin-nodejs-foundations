59461575102972e2341ee9e2bdbde058141d3f5a
irlgabriel <radu.gabriel131@gmail.com>
2020-11-21 09:29:09 +0200

    converted the app to use express

--- basic-info-project/index.js
+++ basic-info-project/index.js
@@ -1,26 +1,25 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  switch(req.url) {
-    case "/":
-      fs.readFile("index.html", (err, data) => {
-        res.write(data)
-        res.end();
-      })
-      break;
-    case "/about":
-      fs.readFile("about.html", (err, data) => {
-        res.write(data)
-        res.end()
-      })
-      break;
-    case "/contact":
-      fs.readFile("contact.html", (err, data) => {
-        res.write(data)
-        res.end();
-      })
-      break;
-  }
-}).listen(8080);
+app.listen(8080, () => console.log("Server started on port 8080"));
+
+const errorHandler = (err, req, res, next) => {
+  if(err) res.sendFile(path.resolve(__dirname, "404.html"));
+  next();
+}
+
+app.use(errorHandler);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "contact.html"));
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "about.html"));
+})
