14a555df0b439cb6b8a1f666754b2d66fd1affe6
irlgabriel <radu.gabriel131@gmail.com>
2020-11-23 18:14:58 +0200

    added CRUD routes for genre and books

---
+++ inventory-application/routes/books.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/books');
+
+// GET all books
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch(err) {
+    res.json(err);
+  }
+})
+// GET book by id
+router.get('/:book_id', async (req, res, next) => {
+  try {
+    const book = await Book.findOne({_id: req.params.book_id});
+    res.json(book);
+  } catch(err) {
+    res.json(err);
+  }
+})
+router.post('/', async (req, res, next) => {
+  const { title, author, pages, isbn, stock, genre, price} = req.body;
+  try {
+    const newBook = await Book.create({
+      title,
+      author,
+      pages,
+      isbn,
+      stock,
+      genre,
+      price
+    })
+    res.json(newBook);
+  } catch(err) {
+    res.json(err);
+  }
+})
+
+router.put('/:book_id', (req, res, next) => {
+  const updatedProps = {}; 
+  Object.keys(req.body).forEach((key, index) => {
+    updatedProps[req.body[index]] = req.body[key];
+  })
+  console.log(updatedProps)
+})
+
+.router.delete('/:book_id', async (req, res, next) => {
+  try {
+    const deletedBook = Book.findOneAndDelete({_id: req.params.book_id});
+    res.json(deletedBook);
+  } catch(err) {
+    res.json(err);
+  }
+})
---
+++ inventory-application/routes/genres.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const Genre = require('../models/genres');
+
+// GET all genres
+router.get('/', async (req, res, next) => {
+  try {
+    const genres = Genre.find();
+    res.json(genres);
+  } catch(err) {
+    res.json(err);
+  }
+})
+// GET Genre by name
+router.get('/:genre_name', async (req, res, next) => {
+  try {
+    const genre = Genre.findOne({name: req.params.genre_name});
+    res.json(genre);
+  } catch(err) {
+    res.json(err)
+  }
+})
+// POST Create new genre
+router.post('/', async (req, res, next) => {
+  try {
+    const newGenre = Genre.create({name: req.body.name});
+    res.json(newGenre);
+  } catch(err) {
+    res.json(err);
+  }
+})
+
+// PUT Edit genre
+router.put('/:genre_id', async (req, res, next) => {
+  try {
+    const updatedGenre = Genre.findOneAndUpdate({_id: req.params.genre_id}, {new: true}, {name: req.body.name});
+    res.json(updatedGenre);
+  } catch(err) {
+    res.json(err);
+  }
+})
+
+router.delete('/:genre_id', async (req, res, next) => {
+  try {
+    const deletedGenre = await Genre.deleteOne({_id: req.params.genre_id});
+    res.json(deletedGenre);
+  } catch(err) {
+    res.json(err);
+  }
+})
