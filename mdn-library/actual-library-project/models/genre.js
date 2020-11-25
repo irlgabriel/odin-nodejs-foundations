58cfa8fc118b811295981238c43e23216fff0702
irlgabriel <radu.gabriel131@gmail.com>
2020-11-25 05:16:12 +0200

    created models

---
+++ mdn-library/actual-library-project/models/genre.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: {type: String, required: true, unique: true, minlength: 3, maxlength: 100},
+
+})
+
+GenreSchema
+.virtual('url')
+.get(function() {
+  return `/genre/${genre._id}`
+})
