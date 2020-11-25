5c7c7c1e4c977b08cddab26fae5fb8db4763ce32
irlgabriel <radu.gabriel131@gmail.com>
2020-11-26 01:18:18 +0200

    added views layouts and gitignore

--- inventory-project/app.js
+++ inventory-project/app.js
@@ -19,6 +19,10 @@ var app = express();
 // security middleware
 app.use(helmet());
 
+// set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
