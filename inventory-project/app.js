b72cb22c2cae1f17091a5e84c769de0c4f611cc4
irlgabriel <radu.gabriel131@gmail.com>
2020-11-26 00:27:25 +0200

    set up mongoose connection and added mandatory libraries

--- inventory-project/app.js
+++ inventory-project/app.js
@@ -1,18 +1,31 @@
+require('dotenv').config
+var helmet = require('helmet');
+var compression = require('compression');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.once('open', () => "Connected to mongoDB");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// security middleware
+app.use(helmet());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(compression());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
