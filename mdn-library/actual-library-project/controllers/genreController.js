b6e053ba3daf717cbd23e937da09bac9c2daffad
irlgabriel <radu.gabriel131@gmail.com>
2020-11-25 19:18:30 -0800

    added view and controller for genre delete

--- mdn-library/actual-library-project/controllers/genreController.js
+++ mdn-library/actual-library-project/controllers/genreController.js
@@ -1,5 +1,6 @@
 const async = require('async');
 const { body, validationResult } = require("express-validator");
+const { render } = require('pug');
 
 var Book = require('../models/book');
 var Genre = require('../models/genre');
@@ -60,12 +61,28 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id).exec(callback)
+        },
+        genre_books: function(callback) {
+            Book.find({genre: req.params.id}).exec(callback)
+        },
+    }, function(err, results) {
+        if(err) return next(err);
+        res.render('genre_delete', {title: "Delete Genre", genre: results.genre, genre_books: results.genre_books});
+    })
+
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+exports.genre_delete_post = function(req, res, next) {
+    var genre_id = req.body.genreid;
+    Genre.findByIdAndDelete(genre_id)
+    .exec(function(err) {
+        if(err) return next(err);
+        res.redirect('/catalog/genres');
+    })
 };
 
 // Display Genre update form on GET.
