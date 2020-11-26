01c783875b5946bf4f45fe04922f55601029ddac
irlgabriel <radu.gabriel131@gmail.com>
2020-11-25 18:36:46 -0800

    added this view and controller'

--- mdn-library/actual-library-project/controllers/authorController.js
+++ mdn-library/actual-library-project/controllers/authorController.js
@@ -1,7 +1,8 @@
 var async = require('async')
+const { body, validationResult } = require('express-validator');
 
 var Author = require('../models/author');
-var book = require('../models/book');
+var Book = require('../models/book');
 
 // Display list of all Authors.
 exports.author_list = function(req, res) {
@@ -22,7 +23,7 @@ exports.author_detail = function(req, res) {
             .exec(callback)
         },
         books: function(callback) {
-            book.find({author: req.params.id}, 'title summary')
+            Book.find({author: req.params.id}, 'title summary')
             .exec(callback)
         },
     }, function(err, results) {
@@ -38,22 +39,89 @@ exports.author_detail = function(req, res) {
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', {title: 'Create Author'})
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+    // Validate and sanitize fields
+    body('first_name').trim().isLength({min: 1}).escape().withMessage('First name must be specified')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters'),
+    body('family_name').trim().isLength({min: 1}).escape().withMessage('Family name must be specifeid')
+    .isAlphanumeric().withMessage('Family name has non-alphanumeric characters'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+    
+    (req, res, next) => {
+
+        // Extract validation errors from request
+        const errors = validationResult(req)
+        
+        if(!errors.isEmpty()) {
+            return res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() })
+        } else {
+            var author = new Author({
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death
+            });
+            author.save(function(err) {
+                if(err) return next(err);
+                res.redirect(author.url);
+            })
+        }
+    }
+]
 
 // Display Author delete form on GET.
-exports.author_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete GET');
-};
+// Display Author delete form on GET.
+exports.author_delete_get = function(req, res, next) {
 
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id).exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.author==null) { // No results.
+            res.redirect('/catalog/authors');
+        }
+        // Successful, so render.
+        res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+    });
+
+};
 // Handle Author delete on POST.
-exports.author_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete POST');
+exports.author_delete_post = function(req, res, next) {
+
+    async.parallel({
+        author: function(callback) {
+          Author.findById(req.body.authorid).exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.body.authorid }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.authors_books.length > 0) {
+            // Author has books. Render in same way as for GET route.
+            res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/catalog/authors')
+            })
+        }
+    });
 };
 
 // Display Author update form on GET.
--- mdn-library/actual-library-project/controllers/genreController.js
+++ mdn-library/actual-library-project/controllers/genreController.js
@@ -1,6 +1,7 @@
 const async = require('async');
+const { body, validationResult } = require("express-validator");
+
 var Book = require('../models/book');
-const genre = require('../models/genre');
 var Genre = require('../models/genre');
 
 // Display list of all Genre.
@@ -28,13 +29,34 @@ exports.genre_detail = function(req, res, next) {
 
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', {title: 'Create Genre'});
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+    //validate and sanitise the name field
+    body('name', 'Genre name required').trim().isLength({ min: 1}).escape(),
+    (req, res, next) =>  {
+        const errors = validationResult(req);
+        var genre = new Genre({name: req.body.name})
+
+        if(!errors.isEmpty()) {
+            res.render('genre_form', {title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        } else {
+            // Data form is valid
+            // Check if this genre already exists
+            Genre.findOne({name: req.body.name})
+            .exec(function(err, found_genre) {
+                if(err) return next(err);
+                if(found_genre) return res.redirect(found_genre.url);
+                genre.save(function(err) {
+                    res.redirect(genre.url);
+                })
+            })
+        }
+    }
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
