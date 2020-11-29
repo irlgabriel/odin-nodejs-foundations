954f2a74f783554eabe33a5e8191e2cd780703f0
irlgabriel <radu.gabriel131@gmail.com>
2020-11-28 19:58:32 -0800

    added routes and controllers for category - all of them!

--- inventory-project/routes/categories.js
+++ inventory-project/routes/categories.js
@@ -1,15 +1,17 @@
+const { body, validationResult } = require('express-validator');
 const express = require('express');
 const router = express.Router();
 
 const Category = require('../models/categories');
 const Item = require('../models/items');
-router.get('/', async(req, res, next) => {
-  try {
-    const categories = await Category.find();
+router.get('/', (req, res, next) => {
+
+  Category.find((err, categories) => {
+    if(err) return next(err);
     res.render('categories_list', {title: "Categories", categories});
-  } catch(err) {
-    return next(err);
-  }
+  });
+    
+
 })
 // GET items from this category
 router.get('/:id/items', (req, res, next) => {
@@ -24,5 +26,33 @@ router.get('/:id/items', (req, res, next) => {
     })
   })
 })
+// GET Category form
+router.get('/new', (req, res, next) => {
+  res.render('category_form', {title: "Create new Category"})
+})
+// POST Category form
+router.post('/new',[
+  body('name').trim().isLength({min: 1}).escape(),
+  (req, res, next) => {
+    Category.create({name: req.body.name}, (err, category) => {
+      res.redirect('/categories');
+    })
+  }
+])
+router.get('/:id/edit', (req, res, next) => {
+  Category.findById(req.params.id, (err, category) => {
+    if(err) return next(err);
+    res.render('category_form', {title: "Edit Category", category});
+  })
+})
+router.post('/:id/edit', [
+  body('name').trim().isLength({min: 1}).escape(),
+  (req, res, next) => {
+    Category.findByIdAndUpdate(req.params.id, {name: req.body.name}, (err, category) => {
+      if(err) return next(err);
+      res.redirect('/categories');
+    })
+  }
+])
 
 module.exports = router;
