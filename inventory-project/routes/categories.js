9cedac51299e2b3016fe562dc06c21013568ca86
irlgabriel <radu.gabriel131@gmail.com>
2020-11-28 18:05:11 -0800

    added view and controllers for category items

--- inventory-project/routes/categories.js
+++ inventory-project/routes/categories.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Category = require('../models/categories');
-
+const Item = require('../models/items');
 router.get('/', async(req, res, next) => {
   try {
     const categories = await Category.find();
@@ -10,6 +10,19 @@ router.get('/', async(req, res, next) => {
   } catch(err) {
     return next(err);
   }
+})
+// GET items from this category
+router.get('/:id/items', (req, res, next) => {
+  Category.findById(req.params.id, (err, category) => {
+    if(err) return next(err);
+    Item.find({category: category._id})
+    .populate('brand')
+    .populate('category')
+    .exec((err, items) => {
+      if(err) return next(err);
+      res.render('category_items', {title: category.name, items, category})
+    })
+  })
 })
 
 module.exports = router;
