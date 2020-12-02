52966ced4dcf5dc1f40403da83ea0f23860c3f75
irlgabriel <radu.gabriel131@gmail.com>
2020-12-02 19:29:46 +0200

    restructured file hierarchy

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import { Container } from 'reactstrap';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  // Fetch resources
+  useEffect((posts) => {
+    // fetch posts
+      
+  }, [])
   return (
-    <></>
+    <Container>
+
+    </Container>
   );
 }
 
