9d069ed11f6e492515d461cbc682690b96e011ac
irlgabriel <radu.gabriel131@gmail.com>
2020-12-04 18:50:41 +0200

    changed to prod url

--- blog/client/src/index.js
+++ blog/client/src/index.js
@@ -6,9 +6,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-//axios.defaults.baseURL = 'https://nodejsblog-api.herokuapp.com/api'
+//prod
+axios.defaults.baseURL = 'https://nodejsblog-api.herokuapp.com/api'
 //dev
-axios.defaults.baseURL = 'http://localhost:5000/api';
+//axios.defaults.baseURL = 'http://localhost:5000/api';
 
 ReactDOM.render(
   <React.StrictMode>
