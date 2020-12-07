d816b75a0c3293f4ffd780690e986fcfb340bf5d
irlgabriel <radu.gabriel131@gmail.com>
2020-12-07 20:25:56 +0200

    merge conflicts

--- blog/server/controllers/posts.js
+++ blog/server/controllers/posts.js
@@ -2,7 +2,6 @@ const { body, validationResult } = require('express-validator');
 
 const Post = require('../models/posts');
 const passport = require('passport');
-const { populate } = require('../models/posts');
 
 exports.get_posts = (req, res, next) => {
   Post.find()
@@ -50,20 +49,20 @@ exports.create_post = [
 ]
 
 exports.edit_post = [
-  body('title').trim().isLength({min: 1}).escape(),
-  body('content').trim().isLength({min: 1}).escape(),
+  body('title').isLength({min: 1}).escape(),
+  body('content').isLength({min: 1}).escape(),
   (req, res, next) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) return res.json(errors.array());
 
     const {title, content} = req.body;
-
-    Post.findOneAndUpdate({_id: req.params.post_id}, {title, content}, {useFindAndModify: false, new: true})
-    .populate('author')
-    .exec((err, updatedPost) => {
-      if(err) return res.json(err);
-      res.json(updatedPost);
+    Post.findOneAndUpdate({_id: req.params.post_id}, {title, content}, {useFindAndModify: false, new: true}, (err, updatedPost) => {
+      if(err) return res.status(400).json(err);
+      updatedPost.populate('author', (err, populatedPost) => {
+        if(err) return res.status(400).json(err);
+        res.json(populatedPost);
+      })
     })
   }
 ]
