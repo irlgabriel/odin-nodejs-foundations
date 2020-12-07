e6eb02cef39f3cd1c5c9c8c564d6d6cf4e6678bf
irlgabriel <radu.gabriel131@gmail.com>
2020-12-06 23:41:05 -0800

    basic configs

--- facebook/server/app.js
+++ facebook/server/app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
-var app = express();
+mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connection.on('open', () => console.log('Connected to mongoDB'));
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
