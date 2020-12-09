55997265a8352dde19886cf697ce4fbd2da613f7
irlgabriel <radu.gabriel131@gmail.com>
2020-12-09 11:50:04 +0200

    created post and comment models

---
+++ facebook/server/models/comments.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  content: String,
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+  post: {type: Schema.Types.ObjectId, ref: 'Post'},
+  comment: {type: Schema.Types.ObjectId, ref: 'Comment'},
+  likes: [{type: Schema.Types.ObjectId, ref: 'User'}]
+})
+
+module.exports = mongoose.model('Comment', commentSchema);
---
+++ facebook/server/models/posts.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  content: String,
+  image: String,
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+  likes: [{type: Schema.Types.ObjectId, ref: 'User'}],
+
+}, {timestamps: true})
+
+module.exports = mongoose.model('Post', postSchema);
