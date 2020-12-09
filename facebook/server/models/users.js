8bd2929991145aedc643f1a50d5c5cab92d34a81
irlgabriel <radu.gabriel131@gmail.com>
2020-12-09 12:34:09 +0200

    added friends field to user model

--- facebook/server/models/users.js
+++ facebook/server/models/users.js
@@ -9,6 +9,7 @@ const UserSchema = new Schema({
   display_name: String,
   first_name: String,
   last_name: String,
+  friends: [{type: Schema.Types.ObjectId, ref: 'User'}]
 }, {timestamps: true});
 
 UserSchema.virtual('full_name').get(function() {
