f921e652c61af72d5ef62d329590dea3a19b29e7
irlgabriel <radu.gabriel131@gmail.com>
2021-01-04 21:23:25 +0200

    fixed register path

--- facebook/client/src/Pages/Register/Register.js
+++ facebook/client/src/Pages/Register/Register.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { Container, Form, Input, Label, FormGroup, Button } from "reactstrap";
 import { FlashMessage } from "./Register.components";
 import { CSSTransition } from 'react-transition-group';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Register = ({ user, reloadUser, getUser }) => {
   const location = useHistory();
@@ -29,7 +28,9 @@ const Register = ({ user, reloadUser, getUser }) => {
         last_name: lastName,
       })
       .then((res) => {
+        localStorage.setItem('token', res.data);
         getUser(res.data);
+        location.push('/');
       })
       .catch((err) => {
         console.log(err.response);
