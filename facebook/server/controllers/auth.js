3fbbbda878a965af80a3341cb732542079856589
irlgabriel <radu.gabriel131@gmail.com>
2021-01-05 15:38:03 +0200

    trying just httpOnly options for token cookie

--- facebook/server/controllers/auth.js
+++ facebook/server/controllers/auth.js
@@ -50,9 +50,9 @@ exports.facebook_callback =
     if(err) return next(err);
     if(!user) return res.redirect(process.env.FRONTEND_URL)
     jwt.sign({user_id: user._id}, process.env.JWT_SECRET, (err, token) => {
-      //res.cookie("token", token, {httpOnly: true, secure: true, sameSite: 'none'});
-      res.setHeader(`Set-Cookie`,  `token=${token}; SameSite=None; Secure`);
-      return res.redirect(process.env.FRONTEND_URL);
+      res.cookie("token", token, {httpOnly: true});
+      //res.setHeader(`Set-Cookie`,  `token=${token}; SameSite=None; Secure`);
+      //return res.redirect(process.env.FRONTEND_URL);
     })
   })(req, res, next);
 }
